7396acb452d556603f880bf03d863cf56e220b83
Anubhav Gupta <proanubhav@gmail.com>
2023-02-06 10:49:19 +0530

    php mailer and logo changes

--- include/js-url.php
+++ include/js-url.php
@@ -2,7 +2,7 @@
 <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.bundle.min.js"></script>
 <!-- Form Validator JS -->
-<script src="assets/js/form-validator.min.js"></script>
+<!-- <script src="assets/js/form-validator.min.js"></script> -->
 <!-- Contact JS -->
 <script src="assets/js/contact-form-script.js"></script>
 <!-- Meanmenu JS -->
@@ -17,3 +17,52 @@
 <script src="assets/js/jquery.magnific-popup.min.js"></script>
 <!-- Custom JS -->
 <script src="assets/js/custom.js"></script>
+
+<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+
+<!-- contact form script start -->
+<script>
+    function submitForm(formId) {
+        debugger
+        try {
+
+            if (!$("#" + formId).valid()) {
+                return false
+            }
+            $("button").attr('disabled', true)
+            var postData = $('#' + formId).serialize()
+
+            $.ajax({
+                type: 'POST',
+                url: "mail.php",
+                    data: postData,
+                    success: function (data) {
+                        $("button").attr('disabled', false)
+                        if (data == 1) {
+                            $("#" + formId)[0].reset();
+                            var x = document.getElementById("snackbar");
+                            x.className = "show";
+                            setTimeout(function () {
+                                x.className = x.className.replace("show", "");
+                            }, 3000);
+                            $("#myModalEnquiry").modal("hide");
+                        } else {
+                            alert(data)
+                        }
+                    },
+                    error: function (error) {
+                        
+                        $("button").attr('disabled', false)
+                        alert("Something went wrong, please try again later")
+                    }
+                });
+            } catch (error) {
+                console.log(error.message);
+    
+                $("button").attr('disabled', false)
+                alert("Something went wrong, please try again later")
+            }
+        }
+    </script>
+    
+    <div id="snackbar">Form Submitted Successfully</div>
---
+++ mail.php
@@ -0,0 +1,81 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+// email Or Phone validation
+
+if(!empty($_POST['email'])) {
+
+if (!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
+    echo "Please provide a valid email-id";
+    return;
+}
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$msg = $_POST['message'];
+
+$phone = isset($_POST['phone']) ? $_POST['phone'] : 'Madover Finance';
+
+
+
+    
+    
+/********************Sending Email******************************************/
+
+$mail = new PHPMailer(true);  // Passing `true` enables exceptions
+try {
+    //Server settings
+    // $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();    
+                                      // Set mailer to use SMTP
+    $mail->Host = 'mail.madoverfinance.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'mail@madoverfinance.com';                 // SMTP username
+    $mail->Password = 'madoverfinance@12345';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+    //Recipients
+    // enquiry@connectinfosys.com
+    $mail->setFrom('proanubhav@gmail.com', 'Mad over Finance Enquiry');
+    $mail->addAddress('proanubhav@gmail.com');     // Add a recipient
+    $mail->isHTML(true); 
+    $mail->Body.='<html lang="en">';
+    $mail->Body.='<head>';
+    $mail->Body.='<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">';
+    $mail->Body.='</head>';
+    $mail->Body.='<body style="font-family: candara;height:auto;">';    
+    $mail->Body.='<div style="background-color: #012f5c; padding:1%;margin:0;border-radius:2px">';
+    $mail->Body.='<p style="color: #fff; text-align: center; font-size: 45px;font-weight:600;margin:0">Welcome to <span style="font-size:55px">Madover Finance</span></p>';
+    $mail->Body.='</div>';
+    $mail->Body.='<div style="border: 5px solid #012f5c; border-top: none;">';
+    $mail->Body.='<div style="padding: 1% 3%;margin: 0 0 1% 0">';
+    $mail->Body.='<p style="font-size: 25px;text-align: center;text-transform:capitalize;font-weight: 800;margin:0">Contact Form</p>';
+    $mail->Body.='<p style="font-size: 16px;"><strong>Hello </strong>Madover Finance,</p>';
+    $mail->Body.='<p style="font-size: 16px;">Someone has tried to contact Madover Finance team.</p>';
+    $mail->Body.='<p style="font-size: 16px;">Name:'.@$name.'</p>';
+    $mail->Body.='<p style="font-size: 16px;">Email:'.@$email.'</p>';
+    $mail->Body.='<p style="font-size: 16px;">Phone:'.@$phone.'</p>';
+    $mail->Body.='<p style="font-size: 16px;">Message Detail:'.@$msg.'</p>';
+
+    $mail->Body.='<p style="font-size: 16px;">Cheers!</p>';
+    $mail->Body.='<p style="font-size: 16px;">Madover Finance</p>';
+    $mail->Body.='</div>';
+    $mail->Body.='</div>';
+    $mail->Body.='</body>';
+    $mail->Body.='</html>';
+    $mail->Subject = 'Madover Finance';
+    $mail->send();
+    echo '1';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
+
+}else{
+    echo "mail not provide";
+}
+?>
